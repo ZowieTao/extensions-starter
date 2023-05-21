4dc071a17e6026a63de3a096524c95c79b354a32
zowietao <zowietao@gmail.com>
2023-05-21 19:43:19 +0800

    chore: chrome.tabs.create

--- github-short-access/service-worker.js
+++ github-short-access/service-worker.js
@@ -2,7 +2,11 @@
 chrome.runtime.onInstalled.addListener(({ reason }) => {
   if (reason === "install") {
     chrome.storage.local.set({
-      gitInfo: { zowietao: {}, opencorddotxyz: {}, openIMSDK: {} },
+      gitInfo: {
+        zowietao: ["extensions-starter"],
+        opencorddotxyz: ["opencord-frontend"],
+        openIMSDK: ["openim-sdk-core"],
+      },
     });
   }
 });
@@ -16,25 +20,26 @@ const NUMBER_OF_PREVIOUS_SEARCHES = 4;
 chrome.omnibox.onInputChanged.addListener(async (input, suggest) => {
   // get history from local
 
-  const [ownerSearchStr, repo, action] = input
+  const [ownerSearchStr, repoSearchStr, action] = input
     .trim()
     .replace(/\s+/g, " ")
     .split(" ");
 
   // first item
   await chrome.omnibox.setDefaultSuggestion({
-    content: `/${[ownerSearchStr, repo, action].join("/")}`,
+    // content: `/${[ownerSearchStr, repoSearchStr, action].join("/")}`,
     description: "Enter keyword separate by blank or choose from past searches",
   });
 
   const { gitInfo } = await chrome.storage.local.get("gitInfo");
   const ownerNames = Object.keys(gitInfo);
 
-  if (ownerSearchStr && !repo) {
-    for (let idx in ownerNames) {
-      const startsWith = ownerNames[idx].startsWith(ownerSearchStr);
+  for (let idx in ownerNames) {
+    const startsWith = ownerNames[idx].startsWith(ownerSearchStr || "");
+    const _ownerName = ownerNames[idx];
+
+    if (ownerSearchStr && !repoSearchStr) {
       if (startsWith || idx + 1 === ownerNames.length) {
-        const _ownerName = ownerNames[idx];
         suggest(
           ownerGenerateSuggestions(
             startsWith ? _ownerName : ownerSearchStr,
@@ -42,21 +47,32 @@ chrome.omnibox.onInputChanged.addListener(async (input, suggest) => {
           )
         );
       }
+    } else if (!ownerSearchStr) {
+      const DefaultOwner = "zowietao";
+      suggest(ownerGenerateSuggestions(DefaultOwner));
+    } else {
+      suggest(
+        ownerGenerateSuggestions(
+          startsWith ? _ownerName : ownerSearchStr,
+          gitInfo[_ownerName],
+          repoSearchStr
+        )
+      );
     }
-  } else if (!ownerSearchStr) {
-    const DefaultOwner = "zowietao";
-    suggest(ownerGenerateSuggestions(DefaultOwner));
-  } else {
-    //
   }
 });
 
 // Open the reference page of the chosen API
-chrome.omnibox.onInputEntered.addListener((input) => {
-  chrome.tabs.create({ url: URL_GITHUB + input });
-  // Save the latest keyword
-  updateHistory(input);
-});
+chrome.omnibox.onInputEntered.addListener(
+  (mediaQueryList, mediaQueryListEvent) => {
+    console.log("entry and input: ", mediaQueryList, mediaQueryListEvent);
+    chrome.tabs.create({
+      url: URL_GITHUB + mediaQueryList.split(" ").join("/"),
+    });
+    // Save the latest keyword
+    // updateHistory(input);
+  }
+);
 
 async function updateHistory(input) {
   const { apiSuggestions } = await chrome.storage.local.get("apiSuggestions");
@@ -65,18 +81,22 @@ async function updateHistory(input) {
   return chrome.storage.local.set({ apiSuggestions });
 }
 
-function ownerGenerateSuggestions(owner, repos) {
+function ownerGenerateSuggestions(owner, repos, repoSearchStr) {
   if (repos?.length > 0) {
-    return repos.map((repoName) => {
-      return {
-        content: `/${owner}/${repoName}`,
-        description: `Owner:${owner}, repo: ${repoName}`,
-      };
-    });
+    return repos
+      .filter((repoName) => {
+        return repoSearchStr ? repoName.startsWith(repoSearchStr) : true;
+      })
+      .map((repoName) => {
+        return {
+          content: `${owner} ${repoName}`,
+          description: `SUGGESTION: Owner: ${owner}, Repo: ${repoName}`,
+        };
+      });
   } else {
     return [
       {
-        content: `/${owner}`,
+        content: `${owner} ${repoSearchStr ? `${repoSearchStr}` : ""}`,
         description: `visit ${owner}'s github`,
       },
     ];
