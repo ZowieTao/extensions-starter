0bde3d2c931808cc9713868b59af297b0e7b99ee
zowie <zowietao@gmail.com>
2023-05-21 18:33:29 +0800

    feat: focus mod

---
+++ focus-mode/background.js
@@ -0,0 +1,37 @@
+const extensionsUrl = "https://developer.chrome.com/docs/extensions";
+const webstoreUrl = "https://developer.chrome.com/docs/webstore";
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.action.setBadgeText({
+    text: "OFF",
+  });
+});
+
+chrome.action.onClicked.addListener(async (tab) => {
+  if (tab.url.startsWith(extensionsUrl) || tab.url.startsWith(webstoreUrl)) {
+    // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
+    const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+    // Next state will always be the opposite
+    const nextState = prevState === "ON" ? "OFF" : "ON";
+
+    // Set the action badge to the next state
+    await chrome.action.setBadgeText({
+      tabId: tab.id,
+      text: nextState,
+    });
+
+    if (nextState === "ON") {
+      // Insert the CSS file when the user turns the extension on
+      await chrome.scripting.insertCSS({
+        files: ["focus-mode.css"],
+        target: { tabId: tab.id },
+      });
+    } else if (nextState === "OFF") {
+      // Remove the CSS file when the user turns the extension off
+      await chrome.scripting.removeCSS({
+        files: ["focus-mode.css"],
+        target: { tabId: tab.id },
+      });
+    }
+  }
+});
